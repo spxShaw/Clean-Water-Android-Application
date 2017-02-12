a35188811c6b351e36c2e00d3345ccb11eb89b8b
spxShaw <sshaw32@gatech.edu>
2017-02-12 09:46:10 -0500

    This directs the user to a Login Screen. The actual Login is not implemented. App ends after putting in Login information. Splashscreen was added before the login screen.

---
+++ app/src/main/java/com/crystal/cleanwaterandroidapplication/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.crystal.cleanwaterandroidapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
