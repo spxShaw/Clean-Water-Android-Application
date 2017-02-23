4d6bacd805968f8d0101e88e1a70c12489bedb4a
Robert Cooper <rlcooper46@gmail.com>
2017-02-23 12:36:46 -0500

    Updated AccountsList.java, adding checkForUsername method

--- app/src/main/java/com/crystal/cleanwaterandroidapplication/AccountsList.java
+++ app/src/main/java/com/crystal/cleanwaterandroidapplication/AccountsList.java
@@ -6,15 +6,12 @@ import java.util.ArrayList;
  * AccountsList stores a list of all accounts in the system.
  * @author
  * @see Account
- * @version 1.0
- * @since 1.0
  */
 public class AccountsList {
     private static ArrayList<Account> list;
 
     /**
      * Creates an AccountList
-     * @since 1.0
      */
     public AccountsList() {
         list = new ArrayList<Account>();
@@ -22,7 +19,6 @@ public class AccountsList {
 
     /**
      * Gets a list of the accounts in the system.
-     * @since 1.0
      * @return ArrayList of accounts
      */
     public ArrayList<Account> getAccountsList() {
@@ -30,9 +26,23 @@ public class AccountsList {
     }
 
     /**
+     * Checks if the username already exists for an account in the AccountList. Returns
+     * true if username exists, false if username does not exist.
+     * @param username username to check
+     * @return True if username exists, false if username does not exist.
+     */
+    public boolean checkForUsername(String username) {
+        for(Account a: list) {
+            if(a.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
      * Adds an account to the system. Returns true if account does not
      * exist and is added, false if account already exists and is not added.
-     * @since 1.0
      * @param newAccount the account to add
      * @return true if account is added, false if account is not added.
      */
@@ -47,7 +57,6 @@ public class AccountsList {
     /**
      * Removes an account from the system by the accounts username. Returns true
      * if account is removed, false if account is not removed (not found).
-     * @since 1.0
      * @param username username of account to remove
      * @return Rrue if account is removed, false if account is not removed.
      */
@@ -64,7 +73,6 @@ public class AccountsList {
     /**
      * Removes an account from the system. Returns true if account is removed,
      * false if account is not removed (not found).
-     * @since 1.0
      * @param account Account to remove.
      * @return True if account is removed, false if account is not removed.
      */
@@ -75,7 +83,6 @@ public class AccountsList {
     /**
      * Validates a set of credentials, seeing if they match an account in the system. Will
      * return true if they have a match, false if they don't have a match.
-     * @since 1.0
      * @param username username of account to validate
      * @param password password of account to validate
      * @return true if account is validated, false if account is not validated
