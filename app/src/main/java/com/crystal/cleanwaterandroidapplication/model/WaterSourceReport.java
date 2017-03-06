fec3d4e299d78093880e8884a5aefbeb0d121180
cpham30 <christopherpham108@gmail.com>
2017-03-05 22:58:33 -0500

    Added WaterSourceReport class.

---
+++ app/src/main/java/com/crystal/cleanwaterandroidapplication/model/WaterSourceReport.java
@@ -0,0 +1,29 @@
+package com.crystal.cleanwaterandroidapplication.model;
+
+/**
+ * Water Source Report.
+ *
+ * @author Team 62
+ */
+
+public class WaterSourceReport {
+
+    private int month, day, year; //date of submission
+    private int hours, seconds, milliseconds; //time of submission
+    private int reportNumber; //auto-generated number assigned to report
+    private String reporterName; //name of reporter from account info
+    private String location; //location is a String for now
+    private WaterType waterType;
+    private WaterCondition watercondition;
+
+    public enum WaterType {
+        BOTTLED, WELL, STREAM, LAKE, SPRING, OTHER
+    }
+
+    public enum WaterCondition {
+        WASTE, TREATABLE_CLEAR, TREATABLE_MUDDY, POTABLE
+    }
+
+
+
+}
