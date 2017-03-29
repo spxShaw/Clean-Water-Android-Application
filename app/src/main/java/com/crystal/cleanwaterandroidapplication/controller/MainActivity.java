379ec3fe012ff5bfaedcd78070b46ab98725be45
Robert Cooper <rlcooper46@gmail.com>
2017-03-28 21:14:37 -0400

    Bam Bam

--- app/src/main/java/com/crystal/cleanwaterandroidapplication/controller/MainActivity.java
+++ app/src/main/java/com/crystal/cleanwaterandroidapplication/controller/MainActivity.java
@@ -1,50 +1,60 @@
 package com.crystal.cleanwaterandroidapplication.controller;
 
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import com.crystal.cleanwaterandroidapplication.model.AccountManager;
 
 import com.crystal.cleanwaterandroidapplication.R;
+import com.crystal.cleanwaterandroidapplication.model.Permission;
 
 public class MainActivity extends AppCompatActivity {
+
+    //UI References
+    private Button logoutButton;
+    private Button editProfileButton;
+    private Button viewReportButton;
+    private Button submitReportButton;
+    private Button viewMapButton;
+    private Button submitWaterQualityReportButton;
+    private View userMainLayout;
+    private View workerMainLayout;
+    private View managerMainLayout;
+    private View administratorMainLayout;
+    private View developerMainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("CleanWaterAndroidApplication");
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         setContentView(R.layout.content_main);
         setSupportActionBar(toolbar);
 
+        /***** BUTTON SETUP *****/
+        logoutButton = (Button) findViewById(R.id.LogOutButton);
+        editProfileButton = (Button) findViewById(R.id.EditProfileButton);
+        viewReportButton = (Button) findViewById(R.id.ViewReportButton);
+        submitReportButton = (Button) findViewById(R.id.SubmitReportButton);
+        viewMapButton = (Button) findViewById(R.id.ViewMapButton);
+        submitWaterQualityReportButton = (Button) findViewById(R.id.SubmitWaterQualityReport);
 
-        Button button = (Button) findViewById(R.id.LogOutButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        logoutButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, WelcomeScreen.class);
+                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
                 startActivity(intent);
             }
         });
 
-        Button button2 = (Button) findViewById(R.id.editProfile);
-        button2.setOnClickListener(new View.OnClickListener() {
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -53,18 +63,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button button3 = (Button) findViewById(R.id.submitReportButton);
-        button3.setOnClickListener(new View.OnClickListener() {
+        viewReportButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SubmitReportActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button button4 = (Button) findViewById(R.id.ViewReportButton);
-        button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ViewReportsActivity.class);
@@ -72,8 +72,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button button5 = (Button) findViewById(R.id.ViewMapButton);
-        button5.setOnClickListener(new View.OnClickListener() {
+        submitReportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubmitReportActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        viewMapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ViewMap.class);
@@ -81,5 +88,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        submitWaterQualityReportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        /***** VIEW SETUP *****/
+        userMainLayout = (View) findViewById(R.id.UserMainLayout);
+        workerMainLayout = (View) findViewById(R.id.WorkerMainLayout);
+        managerMainLayout = (View) findViewById(R.id.ManagerMainLayout);
+        administratorMainLayout = (View) findViewById(R.id.AdministratorMainLayout);
+        developerMainLayout = (View) findViewById(R.id.DeveloperMainLayout);
+
+        if (AccountManager.getCurrentAccount().hasPermission(Permission.USER)) {
+            userMainLayout.setVisibility(View.VISIBLE);
+        } else {
+            userMainLayout.setVisibility(View.INVISIBLE);
+        }
+
+        if (AccountManager.getCurrentAccount().hasPermission(Permission.WORKER)) {
+            workerMainLayout.setVisibility(View.VISIBLE);
+        } else {
+            workerMainLayout.setVisibility(View.INVISIBLE);
+        }
+
+        if (AccountManager.getCurrentAccount().hasPermission(Permission.MANAGER)) {
+            managerMainLayout.setVisibility(View.VISIBLE);
+        } else {
+            managerMainLayout.setVisibility(View.INVISIBLE);
+        }
+
+        if (AccountManager.getCurrentAccount().hasPermission(Permission.ADMINISTRATOR)) {
+            administratorMainLayout.setVisibility(View.VISIBLE);
+        } else {
+            administratorMainLayout.setVisibility(View.INVISIBLE);
+        }
+
+        if (AccountManager.getCurrentAccount().hasPermission(Permission.DEVELOPER)) {
+            developerMainLayout.setVisibility(View.VISIBLE);
+        } else {
+            developerMainLayout.setVisibility(View.INVISIBLE);
+        }
     }
 }
