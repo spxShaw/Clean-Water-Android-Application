8a383c677febe4329fddb11140c18f4604d51c85
spxShaw <sshaw32@gatech.edu>
2017-04-03 14:35:52 -0400

    Added some front end GraphView material. Backend for the graph still needed.

--- app/src/main/java/com/crystal/cleanwaterandroidapplication/controller/TrendsActivity.java
+++ app/src/main/java/com/crystal/cleanwaterandroidapplication/controller/TrendsActivity.java
@@ -1,5 +1,6 @@
 package com.crystal.cleanwaterandroidapplication.controller;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.crystal.cleanwaterandroidapplication.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class TrendsActivity extends AppCompatActivity {
+
+
+    LineGraphSeries<DataPoint> series;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,26 @@ public class TrendsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_trends);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        GraphView graph = (GraphView) findViewById(R.id.trendsGraph);
+        series = new LineGraphSeries<DataPoint>();
+
+        //Insert code to add datapoints to the series
+
+        //series.appendData(new DataPoint());
+
+
+
+        //Graph settings
+        series.setTitle("This is the title");
+        series.setColor(Color.BLUE);
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(10);
+        series.setThickness(8);
+
+
+        graph.addSeries(series);
 
 
     }
