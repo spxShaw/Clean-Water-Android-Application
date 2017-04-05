5a4e5d21e49d4d32cb1ef7beff7b00691faf105a
lucillewang <wonderland21>
2017-04-05 01:24:31 -0400

    Adding Javadocs...

--- app/src/main/java/com/crystal/cleanwaterandroidapplication/model/AccountDoesNotExistException.java
+++ app/src/main/java/com/crystal/cleanwaterandroidapplication/model/AccountDoesNotExistException.java
@@ -6,10 +6,19 @@ package com.crystal.cleanwaterandroidapplication.model;
  * @see AccountManager
  */
 public class AccountDoesNotExistException extends Exception {
+
+    /**
+     * Creates a new AccountDoesNotExistException.
+     */
     public AccountDoesNotExistException() {
         //Do nothing
     }
 
+    /**
+     * Creates a new AccountDoesNotExistException with a unique message which is passed
+     * in as a parameter
+     * @param message the message to be displayed when the exception occurs
+     */
     public AccountDoesNotExistException(String message) {
         super(message);
     }
