9edbcd843bcb1f6863f9a89231813d863d7d9923
Robert Cooper <rlcooper46@gmail.com>
2017-04-05 01:24:37 -0400

    Some Changes (Lucille helped)

--- app/src/main/java/com/crystal/cleanwaterandroidapplication/model/HistoryGraph.java
+++ app/src/main/java/com/crystal/cleanwaterandroidapplication/model/HistoryGraph.java
@@ -3,11 +3,14 @@ package com.crystal.cleanwaterandroidapplication.model;
 import com.google.android.gms.maps.model.LatLng;
 
 public class HistoryGraph {
-    private LatLng location;
     private double ppmPoints[]; //Sorted by month
+    private LatLng location;
     private int year;
+    private HistoryGraphType graphType;
 
-    public HistoryGraph(LatLng location) {
-
+    public HistoryGraph(LatLng location, int year, HistoryGraphType graphType) {
+        this.location = location;
+        this.year = year;
+        this.graphType = graphType;
     }
 }
---
+++ app/src/main/java/com/crystal/cleanwaterandroidapplication/model/HistoryGraphType.java
@@ -0,0 +1,6 @@
+package com.crystal.cleanwaterandroidapplication.model;
+
+
+public enum HistoryGraphType {
+    VIRUS_PPM, CONTAMINANT_PPM;
+}
