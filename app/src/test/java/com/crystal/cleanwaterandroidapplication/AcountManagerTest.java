53459c8535a04842959db1b014dfac407f305b14
cpham30 <christopherpham108@gmail.com>
2017-04-24 19:39:38 -0400

    Started test cases for AccountManager class

--- app/src/test/java/com/crystal/cleanwaterandroidapplication/AcountManagerTest.java
+++ app/src/test/java/com/crystal/cleanwaterandroidapplication/AcountManagerTest.java
@@ -1,8 +1,45 @@
 package com.crystal.cleanwaterandroidapplication;
 
+
+import com.crystal.cleanwaterandroidapplication.model.AccountManager;
+
+import org.junit.Before;
+
+//import org.junit.Test;
+
+
 /**
  * Account manager test -- Chris Pham
  *
  */
 public class AcountManagerTest {
+
+    private AccountManager acctMng;
+
+    @Before
+    public void setup() {
+        acctMng = new AccountManager();
+    }
+
+
+
+    /*
+    @Test
+    public void testAdd() {
+
+        Account newAccount = new Account("user", "pass", Permission.USER);
+
+        //Test successful add
+        Assert.assertTrue(acctMng.add(newAccount));
+        Assert.assertEquals("Account username is wrong", "user", newAccount.getUsername());
+        Assert.assertEquals("Account password is wrong", "pass", newAccount.getPassword());
+        //Assert.assertEquals("Account permission is wrong", Permission.USER, newAccount.getAccountPermission());
+
+        //Test failed add
+        Assert.assertFalse(acctMng.add(newAccount));
+
+
+    }*/
+
+
 }
