bacdad5f4c24b05242bc06e644b6f215a636a409
Robert Cooper <rlcooper46@gmail.com>
2017-04-25 15:41:15 -0400

    Ban!

--- app/src/main/java/com/crystal/cleanwaterandroidapplication/controller/AdminActivity.java
+++ app/src/main/java/com/crystal/cleanwaterandroidapplication/controller/AdminActivity.java
@@ -1,16 +1,28 @@
 package com.crystal.cleanwaterandroidapplication.controller;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.crystal.cleanwaterandroidapplication.R;
+import com.crystal.cleanwaterandroidapplication.model.AccountManager;
+import com.crystal.cleanwaterandroidapplication.model.BannedAccountException;
+import com.crystal.cleanwaterandroidapplication.model.InvalidCredentialsException;
 
 /**
  * Controls the 'Admin' screen of the app.
  * @author Team 62
  */
 public class AdminActivity extends AppCompatActivity {
+
+    private EditText username;
+    private Button banButton;
+    private Button unbanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,66 @@ public class AdminActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        username = (EditText) findViewById(R.id.usernameEnter);
+        banButton = (Button) findViewById(R.id.banButton);
+        unbanButton = (Button) findViewById(R.id.unbanButton);
+
+        //Setup ban button
+        banButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ban();
+
+                Intent intent = new Intent(AdminActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Setup unban button
+        unbanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unban();
+
+                Intent intent = new Intent(AdminActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    public void ban() {
+        String usernameText = username.getText().toString();
+        BanTask banTask = new BanTask(usernameText, "1");
+        banTask.execute((Void) null);
+    }
+
+    public void unban() {
+        String usernameText = username.getText().toString();
+        BanTask banTask = new BanTask(usernameText, "0");
+        banTask.execute((Void) null);
+    }
+
+    /*
+     * Login thread. Implemented as a thread for future usability.
+     */
+    private class BanTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String username;
+        private final String ban;
+
+        BanTask(String username, String ban) {
+            this.username = username;
+            this.ban = ban;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            //Ban account or unban account
+            AccountManager.setBan(username, ban);
+            return true;
+        }
     }
 
 }
--- app/src/main/java/com/crystal/cleanwaterandroidapplication/controller/SubmitQualityReportActivity.java
+++ app/src/main/java/com/crystal/cleanwaterandroidapplication/controller/SubmitQualityReportActivity.java
@@ -1,8 +1,14 @@
 package com.crystal.cleanwaterandroidapplication.controller;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -14,11 +20,23 @@ import com.crystal.cleanwaterandroidapplication.R;
 import com.crystal.cleanwaterandroidapplication.model.WaterCondition;
 import com.crystal.cleanwaterandroidapplication.model.WaterReportManager;
 import com.crystal.cleanwaterandroidapplication.model.WaterType;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 /**
  * Activity for controlling the submission of WaterQualityReports.
  */
-public class SubmitQualityReportActivity extends AppCompatActivity {
+public class SubmitQualityReportActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     //UI Reference
     private TextView ReportID;
@@ -29,6 +47,8 @@ public class SubmitQualityReportActivity extends AppCompatActivity {
     private TextView LongitudeTextView;
     private TextView VirusPPMView;
     private TextView ContaminantPPMView;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
 
 
     @Override
@@ -72,7 +92,57 @@ public class SubmitQualityReportActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //Setup google API for getting location
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            //return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            LatitudeTextView.setText(String.valueOf(mLastLocation.getLatitude()));
+            LongitudeTextView.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
 
     private class AddQualityReportTask extends AsyncTask<String, String, String> {
         @Override
