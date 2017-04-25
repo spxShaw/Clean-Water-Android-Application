d08e576764bdf39aa61070e32b8591516e44262a
cpham30 <christopherpham108@gmail.com>
2017-04-25 01:42:44 -0400

    Changes to WaterReportManagerTest.java

--- app/src/test/java/com/crystal/cleanwaterandroidapplication/WaterReportManagerTest.java
+++ app/src/test/java/com/crystal/cleanwaterandroidapplication/WaterReportManagerTest.java
@@ -4,5 +4,36 @@ package com.crystal.cleanwaterandroidapplication;
  * Created by christopherpham on 4/25/17.
  */
 
+import com.crystal.cleanwaterandroidapplication.model.ReportDoesNotExistException;
+import com.crystal.cleanwaterandroidapplication.model.WaterReportManager;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class WaterReportManagerTest {
+    WaterReportManager man;
+
+    @Before
+    public void setup() {
+        man = new WaterReportManager();
+    }
+
+    /**
+     * Testing addReport
+     */
+    @Test
+    public void testAddReport() {
+
+    }
+
+
+    @Test(expected = ReportDoesNotExistException.class)
+    public void testGetReport() {
+        try {
+            man.getReport(32);
+        } catch (ReportDoesNotExistException e) {
+
+        }
+    }
+
 }
